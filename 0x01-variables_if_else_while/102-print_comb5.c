89e19e038d2e30d6941f620b6942e2b855c94481
root <barkotka@gmail.com>
2021-08-27 14:56:46 -0700

    Low programming

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,51 +1,37 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
+ * main - prints all possible combinations of two two-digit numbers
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-int i = '0';
-int j = '0';
-int y = '0';
-int z = '1';  
-while (i < '9')
-{
-while (j < '9')
-{
-while (y < '9')
-{
-while (z < '9')
-{
-putchar(i);
-putchar(j);
-putchar(' ');
-putchar(y);
-putchar(z);
-if (!(i == '8' &&
-j == '7' &&
-y == '8' &&
-z == '8'))
-{
-putchar(',');
-putchar(' ');
-}
-z++;
-}
-y++;
-z = '0';
-}
-j++;
-y = i;
-z = j + 1;
-}
-i++;
-j = '0';
-y = i;
-z = j + 1;
-}
-putchar('\n');
-return (0);
+	int p, q, p1, p2, q1, q2;
+
+	for (p = 0; p < 99; p++)
+	{
+		p1 = p / 10;
+		p2 = p % 10;
+		for (q = 1; q < 100; q++)
+		{
+			q1 = q / 10;
+			q2 = q % 10;
+			if (p < q)
+			{
+				putchar(p1 + '0');
+				putchar(p2 + '0');
+				putchar(' ');
+				putchar(q1 + '0');
+				putchar(q2 + '0');
+				if (p < 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
