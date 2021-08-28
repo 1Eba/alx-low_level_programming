ceb77dbb96dbc58363277248ddc33df88fb8317b
root <barkotka@gmail.com>
2021-08-28 15:01:14 -0700

    low programming

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - Print single digits
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int m;
-
-	for (m = 48; m < 58; m++)
-	{
-		putchar(m);
-		if (m != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+int d = 48;
+while (d < 58)
+{
+putchar(d);
+if (d != 57)
+{
+putchar(',');
+putchar(' ');
+}
+d++;
+}
+putchar('\n');
+return (0);
 }
