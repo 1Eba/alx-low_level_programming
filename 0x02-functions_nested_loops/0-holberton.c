78b7e7615e2c6a82634082115ee59df484980bcc
root <barkotka@gmail.com>
2021-08-31 09:01:54 -0700

    Functions loops

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,20 +1,25 @@
+#include <stdio.h>
 #include "holberton.h"
 
 /**
- * main - Entry point
+ * main- Entry point
  *
- * putchar - to print "holberton"
+ * _putchar: Calls function _putchar
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char holberton[9] = "Holberton";
-int i;
-for (i = 0; i < 9; i++)
-{
-_putchar(holberton[i]);
-}
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
 _putchar('\n');
+
 return (0);
 }
