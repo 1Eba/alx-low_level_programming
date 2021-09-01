386cbfeddbafb5dc4878a9dbb376969bd9a0626b
root <barkotka@gmail.com>
2021-09-01 11:20:02 -0700

    Functions loops

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -12,18 +12,4 @@
 int add(int a, int b)
 {
 	return (a + b);
-}#include "holberton.h"
-
-/**
- * add - Adds two integers and returns
- * the result.
- *
- * @a: number one.
- * @b: number two.
- *
- * Return: Add of number one and number two.
- */
-int add(int a, int b)
-{
-	return (a + b);
 }
