11f23b25b87ed8baba8c59bbd3e94ab2bf2333b8
root <barkotka@gmail.com>
2021-09-02 09:09:40 -0700

    More functions

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,25 @@
 #include "holberton.h"
 
 /**
- * print_line - print line
- * @n: input value
+ * print_line - prints a line unless n is less than 0
  *
- * Description:  draws a straight line in the terminal
- * Return: Always (0)
+ * @n: there will be underscores printed for n times
  */
 
 void print_line(int n)
 {
-	int i;
+	int l;
 
-	if (n > 0)
+	if (n <= 0)
 	{
-		for (i = 0; i < n; i++)
-		{
-			_putchar('_');
-		}
+		_putchar('\n');
 	}
+	else
+	{
+		for (l = 1; l <= n; l++)
+		{
+			_putchar(95);
+		}
 	_putchar('\n');
+	}
 }
