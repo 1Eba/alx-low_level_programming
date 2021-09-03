2ec582f3d3f6c06c3e196eec697ba2e885e1de92
root <barkotka@gmail.com>
2021-09-03 14:54:21 -0700

    More functions

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,32 +1,21 @@
 #include "main.h"
 
 /**
- * print_line - main entry
- * Description - A function that draws a straight line in the terminal
- * You can only use _putchar function to print
- * Where n is the number of times the character _ should be printed
- * The line should end with a \n
- * If n is 0 or less, the function should only print \n
- * *@n: the function accepts an input saved into n
- * from 0 to 14, followed by a new line
- * Return - Nothing (Success)
-*/
+ * print_line - check for a digit
+ * @n : number of _ to be printed
+ * Return:void
+ */
+
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		int j = 1;
 
-		while (j <= n)
-		{
-			_putchar('_');
-			j++;
-		}
+	int i = 0;
 
-		_putchar('\n');
+	while (i < n && n > 0)
+	{
+		_putchar('_');
+		i++;
 	}
+	_putchar('\n');
+
 }
