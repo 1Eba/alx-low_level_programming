de64d314bc183b03c1b15e078d2a908fc875e67d
root <barkotka@gmail.com>
2021-09-06 06:06:25 -0700

    pointers arrays strings

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+#include "holberon.h"
+
+/**
+ * _atoi - converts a string to an integer.
+ * @s: string
+ * Return: int
+ */
+
+int _atoi(cahr *s)
+{
+int sign = 1, i;
+unsigned int num = 0;
+for (i = 0; s[i]; i++)
+{
+if (s[i] == '-')
+sign *= -1;
+else if (s[i] >= '0' && s[i] <= '9')
+num = (num * 10) + (s[i] - '0');
+else if (num > 0)
+{
+break;
+}
+}
+return (num *sign);
+}
