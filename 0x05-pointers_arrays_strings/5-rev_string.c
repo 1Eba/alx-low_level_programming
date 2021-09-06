f5c782784b1febe504a458d0c1c8b796dca63cfc
root <barkotka@gmail.com>
2021-09-06 05:11:15 -0700

    Pointers arrays strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,26 +1,28 @@
 #include "holberton.h"
 
 /**
- * rev_string - reverse a string
+ * rev_string - reverse string
  *
- * Descriptions: reverses a string.
- * Return: Always (0)
+ * @s: input string
+ *
+ * Rerturn: Void
  */
 void rev_string(char *s)
 {
-  int left, i, temp;
-
-  left =0;
-  i = 0;
-  while (s[i] != '\0')
-  {
-    i++;
-  }
-  i--;
-  while (i > left)
-  {
-    temp = s[i];
-    s[i--] = s[left];
-    s[left++] = temp;
-  }
+int leng1 = 0;
+int leng2 = 0;
+char tmp;
+while (s[leng1] != '\0')
+{
+leng1++;
+}
+leng1 = leng1 - 1;
+while (leng2 <= leng1)
+{
+tmp = s[leng2];
+s[leng2] = s[leng1];
+s[leng1] = tmp;
+leng2++;
+leng1--;
+}
 }
