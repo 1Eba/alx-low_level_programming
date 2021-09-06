8e2b250ab3425ad81b9ddda81cec2898b92ec265
root <barkotka@gmail.com>
2021-09-06 07:24:08 -0700

    Pointers arrays strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,6 @@
-#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void)
 {
@@ -16,6 +18,7 @@ sum = sum + finalString[i];
 i++;
 }
 finalString[i] = (2772 - sum) - 0;
+finalString[i + 1] = '\0';
 printf("%s", finalString);
 return (0);
 }
