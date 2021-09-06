8542431cf7f34012b74084b7fc7672613ec85bc1
root <barkotka@gmail.com>
2021-09-06 07:12:47 -0700

    Pointers arrays strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,7 +5,7 @@
 int main(void)
 {
 int i, sum, randomNumber;
-char *originalString = "abcdefghijklmnopqrstuvwxyz";
+char *originalString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ0123456789";
 char finalString[300];
 
 srand(time(NULL));
@@ -17,8 +17,6 @@ finalString[i] = originalString[randomNumber];
 sum = sum + finalString[i];
 i++;
 }
-finalString[i] = (2772 - sum) - 0;
-finalString[i + 1] = '\0';
 printf("%s", finalString);
 return (0);
 }
