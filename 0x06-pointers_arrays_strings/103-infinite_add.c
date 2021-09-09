5e3fe51dd70987eebaa2afd47e1a05cb2c58f237
root <barkotka@gmail.com>
2021-09-09 15:17:04 -0700

    Pointers arrays strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,55 +1,108 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
- * infinite_add - adds two numbers
- * @n1: number one.
- * @n2: number two.
- * @r: buffer that the function will use to store the result.
- * @size_r: buffer size:
- * Return: the pointer to dest.
+ * infinite_add - Add 2 integers.
+ *
+ * @n1: integer
+ *
+ * @n2: integer
+ *
+ * @r: buffer
+ *
+ * size_r: size of r
+ *
+ * Return: char
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+	int c = 0, y = 0, z;
+	char *s1 = n1, *s2 = n2;
 
-while (*(n1 + c1) != '\0')
-c1++;
-while (*(n2 + c2) != '\0')
-c2++;
-if (c1 >= c2)
-bg = c1;
-else
-bg = c2;
-if (size_r <= bg + 1)
-return (0);
-r[bg + 1] = '\0';
-c1--, c2--, size_r--;
-dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
-while (bg >= 0)
-{
-op = dr1 + dr2 + add;
-if (op >= 10)
-add = op / 10;
-else
-add = 0;
-if (op > 0)
-*(r + bg) = (op % 10) + 48;
-else
-*(r + bg) = '0';
-if (c1 > 0)
-c1--, dr1 = *(n1 + c1) - 48;
-else
-dr1 = 0;
-if (c2 > 0)
-c2--, dr2 = *(n2 + c2) - 48;
-else
-dr2 = 0;
-bg--, size_r--;
-}
-if (*(r) == '0')
-return (r + 1);
-else
-return (r);
+	while (*s1 != 0)
+		s1++;
+	while (*s2 != 0)
+		s2++;
+	size_r--;
+	r[size_r] = 0;
+	s1--;
+	s2--;
+
+	while (s2 != n2 - 1 && s1 != n1 - 1)
+	{
+		r[y] = *s2 - '0' + *s1 + c;
+		c = 0;
+
+		if (r[y] > '9')
+		{
+			c++;
+			r[y] -= 10;
+		}
+
+		y++;
+		s2--;
+		s1--;
+
+		if (size_r == y && (s1 != n1 - 1 || s2 != n2 - 1 || c == 1))
+			return (0);
+	}
+
+	while (s1 != n1 - 1)
+	{
+		r[y] = *s1 + c;
+		c = 0;
+
+		if (r[y] > '9')
+		{
+			c = 1;
+			r[y] -= 10;
+		}
+
+		s1--;
+		y++;
+
+		if (size_r == y && (s1 != n1 - 1 ||  c == 1))
+			return (0);
+	}
+	while (s2 != n2 - 1)
+	{
+		r[y] = *s2 + c;
+		c = 0;
+
+		if (r[y] > '9')
+		{
+			c = 1;
+			r[y] -= 10;
+		}
+
+		s2--;
+		y++;
+
+		if (size_r == y && (s2 != n2 - 1 || c == 1))
+			return (0);
+	}
+
+	if (c == 1)
+	{
+		r[y] = '1';
+		r[y + 1] = 0;
+	}
+
+	else
+	{
+		r[y--] = 0;
+	}
+
+	z = 0;
+
+	while (z <= y)
+	{
+		c = r[y];
+		r[y] = r[z];
+		r[z] = c;
+		y--;
+		z++;
+	}
+
+	return (r);
 }
