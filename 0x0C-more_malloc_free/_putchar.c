ea7cc0626a22df02bbf513bcedf5870cd241cc3e
root <barkotka@gmail.com>
2021-09-23 17:23:29 -0700

    More malloc, free

--- 0x0C-more_malloc_free/_putchar.c
+++ 0x0C-more_malloc_free/_putchar.c
@@ -1,13 +1,14 @@
-#include "holberton.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
-* - 
-* @:
-* Return:
-*/
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
+int _putchar(char c)
 {
-return (0);
+	return (write(1, &c, 1));
 }
