f495c551674080a4f1f899ec892a0fe89cb9ed6d
1Eba <barkotka@gmail.com>
2021-09-27 23:03:36 +0000

    Structures, typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 /**
 * print_dog - print the structure of dog
-* @d: structure of the dog
-* Return: Nothing
+*@d: structure of the dog
+*Return: Nothing
 */
 void print_dog(struct dog *d)
 {
@@ -22,7 +22,7 @@ else
 printf("Name: %s\n", d->name);
 }
 printf("Age: %f\n", d->age);
-if (d->owner ==NULL)
+if (d->owner == NULL)
 {
 printf("Owner: (nil)\n");
 }
