5def4e1a7512a8b6d6d21bcb9422cb76fdbb9578
1Eba <barkotka@gmail.com>
2021-11-25 22:57:31 +0000

    Doubly linked lists

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,49 +1,53 @@
 #include "lists.h"
+
 /**
- * delete_dnodeint_at_index - delete node
- * @head: pointer to structure
- * @index: index of the node
+ * delete_dnodeint_at_index - a function that deletes the node at
+ * index index of a dlistint_t linked list
+ * @head: head node
+ * @index:  the index of the node that should be deleted. Index starts at 0
+ *
  * Return: 1 if it succeeded, -1 if it failed
  */
+
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *temp, *temp2;
-	unsigned int count, count2;
+	dlistint_t *curr_node = *head;
+	size_t size = 0;
 
-	if (*head)
+	if (!*head)
 	{
-		temp = *head;
-		for (count2 = 0; temp != NULL; count2++)
-			temp = temp->next;
-		if (index < count2)
-		{
-			temp = *head;
-			temp2 = (*head)->next;
-			if (index == 0)
-			{
-				*head = (*head)->next;
-				free(temp);
-				if (*head)
-					(*head)->prev = NULL;
-			}
-			else
-			{
-				for (count = 0; count < (index - 1); count++)
-				{
-					temp = temp->next;
-					temp2 = temp->next;
-				}
-				temp->next = temp2->next;
-				if (temp->next != NULL)
-					temp2->prev = temp;
-				if (count == (index - 1))
-					free(temp2);
-			}
-		}
-		else
-			return (-1);
-	return (1);
-	}
-	else
 		return (-1);
+	}
+	while (curr_node)
+	{
+		size++;
+		curr_node = curr_node->next;
+	}
+	if (size < index + 1)
+	{
+		return (-1);
+	}
+	curr_node = *head;
+	if (!index)
+	{
+		*head = curr_node->next;
+		if (curr_node->next)
+		{
+			curr_node->next->prev = NULL;
+		}
+		curr_node->next = NULL;
+		free(curr_node);
+		return (1);
+	}
+	while (index--)
+	{
+		curr_node = curr_node->next;
+	}
+	curr_node->prev->next = curr_node->next;
+	if (curr_node->next)
+	{
+		curr_node->next->prev = curr_node->prev;
+	}
+	free(curr_node);
+	return (1);
 }
