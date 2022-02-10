f59f2c21a61df1e542e2e63356799e839218734a
root <1Eba>
2022-02-09 16:14:58 -0800

    grid create function

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-"""
-Island Perimeter
-"""
+
+"""File representing an island"""
+
 
 def island_perimeter(grid):
-    """returns the perimeter of the island described in grid
-    Args:
-        grid(matrix): input grid
-    Description:
-        Traverse the land mass, and
-        for the lake's nearest neighbor
-        then return the total length
     """
-    y = 0
-    x = 0
-    
-    for row in range(1, len(grid) - 1):
-            for col in range(1, len(grid[row]) - 1):
-                if grid[row][col] == 1:
-                    if grid[row][col - 1] == 0:
-                        y += 1
-                    if grid[row][col + 1] == 0:
-                        y += 1
-                    if grid[row - 1][col] == 0:
-                        x += 1
-                    if grid[row + 1][col] == 0:
-                        x += 1
-    return x + y
+    Returns the perimeter of an island.
+    Perimeter is 0 if not 1 finded in the array, means there is no island,
+    each time it has a land near it reduce the perimeter value by one or
+    like the same way opposite if there is water add one
+    """
+    perimeter = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0:
+                    perimeter += 1
+                if row == len(grid) - 1 or grid[row + 1][col] == 0:
+                    perimeter += 1
+                if col == 0 or grid[row][col - 1] == 0:
+                    perimeter += 1
+                if col == len(grid[row]) - 1 or grid[row][col + 1] == 0:
+                    perimeter += 1
+    return (perimeter)
